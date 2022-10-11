31dffe22e8cbe2d4b24dd7a3e2e78070410330bf
Salman Setiawan <11191066@student.itk.ac.id>
2022-10-11 19:16:03 +0800

    update halaman struk belanja

---
+++ lib/struk/struk.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:papb/payment/detailprice.dart';
+
+class StrukBelanja extends StatefulWidget {
+  const StrukBelanja({super.key});
+
+  @override
+  State<StrukBelanja> createState() => _StrukBelanjaState();
+}
+
+class _StrukBelanjaState extends State<StrukBelanja> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber[50],
+      body: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  SizedBox(height: 64),
+                  Text(
+                    "Pesanan Berhasil Dibuat",
+                    style: GoogleFonts.plusJakartaSans(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Kamu akan mendapat notifikasi saat minuman selesai dibuat",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.plusJakartaSans(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Detail Pesanan",
+                  style: GoogleFonts.plusJakartaSans(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: 1000,
+              height: 460,
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 3,
+                    blurRadius: 8,
+                    offset: Offset(0, 1),
+                  ),
+                ],
+              ),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    DetailPrice(),
+                    SizedBox(height: 8),
+                    DetailPrice(),
+                    SizedBox(height: 24),
+                    Text(
+                      "Pesanan",
+                      style: GoogleFonts.plusJakartaSans(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(height: 4),
+                    Row(
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "No",
+                              style: GoogleFonts.plusJakartaSans(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              "1",
+                              style: GoogleFonts.plusJakartaSans(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                            SizedBox(height: 4),
+                            Text(
+                              "2",
+                              style: GoogleFonts.plusJakartaSans(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w400,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 24,
+                        ),
+                        Container(
+                          width: 268,
+                          child: Column(
+                            children: [
+                              DetailPrice(),
+                              SizedBox(height: 4),
+                              DetailPrice(),
+                              SizedBox(height: 4),
+                              DetailPrice(),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 24),
+                    DetailPrice(),
+                    SizedBox(height: 8),
+                    DetailPrice(),
+                    SizedBox(height: 8),
+                    DetailPrice(),
+                    SizedBox(height: 8),
+                    DetailPrice(),
+                  ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
