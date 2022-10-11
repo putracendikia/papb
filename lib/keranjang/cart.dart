3d25be64ffabdcf956dae5c5c9d5201c46962309
Salman Setiawan <11191066@student.itk.ac.id>
2022-10-11 22:22:23 +0800

    update komponen cardbutton

--- lib/keranjang/cart.dart
+++ lib/keranjang/cart.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:papb/keranjang/cartlist.dart';
+import 'package:papb/komponen/cardbutton.dart';
 
 class Cart extends StatefulWidget {
   const Cart({super.key});
@@ -28,42 +29,7 @@ class _CartState extends State<Cart> {
             padding: const EdgeInsets.all(20.0),
             child: Column(
               children: [
-                Container(
-                  width: 1000,
-                  height: 78,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.1),
-                        spreadRadius: 3,
-                        blurRadius: 8,
-                        offset: Offset(0, 1),
-                      ),
-                    ],
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Ngopee Km.10',
-                          style: GoogleFonts.plusJakartaSans(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w800,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          'No.24 Jalan Soekarno Hatta Km.10',
-                          style: GoogleFonts.plusJakartaSans(fontSize: 12),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                CardButton(),
                 SizedBox(height: 32),
                 Column(
                   children: [
@@ -105,43 +71,7 @@ class _CartState extends State<Cart> {
                         ),
                       ),
                       SizedBox(height: 16),
-                      Container(
-                        width: 1000,
-                        height: 78,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          color: Colors.white,
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.1),
-                              spreadRadius: 3,
-                              blurRadius: 8,
-                              offset: Offset(0, 1),
-                            ),
-                          ],
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Diskon 50% untuk anak baru',
-                                style: GoogleFonts.plusJakartaSans(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w800,
-                                ),
-                              ),
-                              SizedBox(height: 8),
-                              Text(
-                                'Tanpa min pembelian untuk semua minuman...',
-                                style:
-                                    GoogleFonts.plusJakartaSans(fontSize: 12),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
+                      CardButton(),
                     ],
                   ),
                 )
--- lib/payment/payment.dart
+++ lib/payment/payment.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:papb/payment/paymentbox.dart';
+import 'package:papb/komponen/cardbutton.dart';
 import 'package:papb/payment/deliverytable.dart';
 import 'package:papb/payment/detailprice.dart';
+import 'package:papb/payment/paymentbox.dart';
 
 class PaymentMenu extends StatefulWidget {
   const PaymentMenu({super.key});
@@ -116,42 +117,7 @@ class _PaymentMenuState extends State<PaymentMenu> {
           SizedBox(
             height: 32,
           ),
-          Container(
-            width: 1000,
-            height: 78,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
-                  spreadRadius: 3,
-                  blurRadius: 8,
-                  offset: Offset(0, 1),
-                ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Ngopee Km.10',
-                    style: GoogleFonts.plusJakartaSans(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w800,
-                    ),
-                  ),
-                  SizedBox(height: 8),
-                  Text(
-                    'No.24 Jalan Soekarno Hatta Km.10',
-                    style: GoogleFonts.plusJakartaSans(fontSize: 12),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          CardButton(),
           SizedBox(
             height: 32,
           ),
