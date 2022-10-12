d56320c8f879907657063ede298fd8d4dfcb3da1
Salman Setiawan <11191066@student.itk.ac.id>
2022-10-12 13:21:23 +0800

    revisi halaman produk

--- lib/produk/coffeearticle.dart
+++ lib/produk/coffeearticle.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:papb/produk/coffeetile.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 class CoffeeArticle extends StatefulWidget {
   const CoffeeArticle({super.key});
@@ -17,10 +18,21 @@ class _CoffeeArticleState extends State<CoffeeArticle> {
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.transparent,
+          leading: GestureDetector(
+            onTap: () {},
+            child: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            ),
+          ),
           centerTitle: true,
           title: Text(
             "Produk A",
-            style: TextStyle(color: Colors.black),
+            style: GoogleFonts.plusJakartaSans(
+              fontSize: 14,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
           ),
         ),
         body: SafeArea(
@@ -31,7 +43,7 @@ class _CoffeeArticleState extends State<CoffeeArticle> {
                 Container(
                   width: double.infinity,
                   child: Image.asset(
-                    'assets/image/produk_a.JPG',
+                    'lib/assets/image/produk_a.JPG',
                     height: 300,
                     fit: BoxFit.cover,
                   ),
@@ -69,19 +81,87 @@ class _CoffeeArticleState extends State<CoffeeArticle> {
                               fontWeight: FontWeight.w800,
                             ),
                           ),
+                          SizedBox(width: 24),
+                          ToggleSwitch(
+                            activeBorders: [
+                              Border.all(
+                                color: Colors.brown,
+                                width: 2.0,
+                              ),
+                              Border.all(
+                                color: Colors.brown,
+                                width: 2.0,
+                              ),
+                            ],
+                            activeFgColor: Colors.black,
+                            activeBgColor: [Colors.transparent],
+                            inactiveBgColor: Colors.transparent,
+                            isVertical: false,
+                            minWidth: 80.0,
+                            radiusStyle: true,
+                            cornerRadius: 8.0,
+                            initialLabelIndex: 2,
+                            labels: ['Hot', 'Cold'],
+                            customTextStyles: [
+                              GoogleFonts.plusJakartaSans(
+                                  fontSize: 14.0, fontWeight: FontWeight.w800),
+                              GoogleFonts.plusJakartaSans(
+                                  fontSize: 14.0, fontWeight: FontWeight.w800),
+                            ],
+                            onToggle: (index) {
+                              print('switched to: $index');
+                            },
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 24,
+                      ),
+                      Row(
+                        children: [
                           Text(
-                            "Hot",
+                            "Ukuran",
                             style: GoogleFonts.plusJakartaSans(
                               fontSize: 14,
                               fontWeight: FontWeight.w800,
                             ),
                           ),
-                          Text(
-                            "Cold",
-                            style: GoogleFonts.plusJakartaSans(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w800,
-                            ),
+                          SizedBox(width: 24),
+                          ToggleSwitch(
+                            activeBorders: [
+                              Border.all(
+                                color: Colors.brown,
+                                width: 2.0,
+                              ),
+                              Border.all(
+                                color: Colors.brown,
+                                width: 2.0,
+                              ),
+                              Border.all(
+                                color: Colors.brown,
+                                width: 2.0,
+                              ),
+                            ],
+                            activeFgColor: Colors.black,
+                            activeBgColor: [Colors.transparent],
+                            inactiveBgColor: Colors.transparent,
+                            isVertical: false,
+                            minWidth: 60.0,
+                            radiusStyle: true,
+                            cornerRadius: 8.0,
+                            initialLabelIndex: 3,
+                            labels: ['S', 'M', 'L'],
+                            customTextStyles: [
+                              GoogleFonts.plusJakartaSans(
+                                  fontSize: 14.0, fontWeight: FontWeight.w800),
+                              GoogleFonts.plusJakartaSans(
+                                  fontSize: 14.0, fontWeight: FontWeight.w800),
+                              GoogleFonts.plusJakartaSans(
+                                  fontSize: 14.0, fontWeight: FontWeight.w800),
+                            ],
+                            onToggle: (index) {
+                              print('switched to: $index');
+                            },
                           ),
                         ],
                       ),
